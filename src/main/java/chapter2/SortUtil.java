7d45b76a6b073bd5a17d3bf14d39c6f9fb0ad906
“frode <lvfangyuan@outlook.com>
2018-09-19 17:54:28 +0800

    commit queue

---
+++ src/main/java/chapter2/SortUtil.java
@@ -0,0 +1,28 @@
+package chapter2;
+
+/**
+ * @Author: frode
+ * @Date: 2018/8/22 18:21
+ * @Description:
+ */
+public class SortUtil {
+    //选择排序
+    public static void selectedSort(int[] arr){
+        int min = arr[0];//第一个数取
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+
+            }
+        }
+    }
+
+    //基本类型改值
+    public static void main(String[] args) {
+        int[] arr = {3,1,5,3,7};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
